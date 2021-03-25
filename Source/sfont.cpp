0e201c71787064b7e1de6e85ba4f2f7ffc2859dc
KKQ-KKQ <KKQ-KKQ@users.noreply.github.com>
2021-03-25 18:18:44 +0900

    Fix expand bug.

--- Source/sfont.cpp
+++ Source/sfont.cpp
@@ -913,6 +913,7 @@ void SoundFont::writeIfil()
     write("ifil", 4);
     writeDword(4);
     unsigned char data[4];
+    if (_fileFormatOut == SF2Format) _version.major = 2;
     if (_fileFormatOut == SF3Format || _fileFormatOut == SF3FormatFlac)
         _version.major = 3;
     if (_fileFormatOut == SF4Format) _version.major = 4;
